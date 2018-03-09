f0f216ea1dde799afbeffff6accd0f5b06b21083
Benjamin Sisson <benmsisson@gmail.com>
2018-03-09 15:24:09 -0600

    Now serializing walls

--- Assets/Scripts/FlockControl.cs
+++ Assets/Scripts/FlockControl.cs
@@ -42,13 +42,22 @@ public class FlockControl : MonoBehaviour {
 	[System.Serializable]
 	struct WorldState {
 		public BirdControl.Bird[] birds;
+		public WallState[] walls;
 		public Vector2 goalPosition;
 	}
 
-	public void Start () {
+	[System.Serializable]
+	struct WallState {
+		public Vector2 topLeft;
+		public Vector2 topRight;
+		public Vector2 bottomLeft;
+		public Vector2 bottomRight;
+	}
+
+	public void Start() {
 		// Set the background based on room settings
-		background.transform.position = new Vector3(ROOM_WIDTH/2,ROOM_HEIGHT/2,5);
-		background.transform.localScale = new Vector3(ROOM_WIDTH+5,ROOM_HEIGHT+5,1);
+		background.transform.position = new Vector3(ROOM_WIDTH / 2, ROOM_HEIGHT / 2, 5);
+		background.transform.localScale = new Vector3(ROOM_WIDTH + 5, ROOM_HEIGHT + 5, 1);
 		background.GetComponent<Renderer>().material.color = Color.black;
 
 		goal = Instantiate<GameObject>(goalPrefab);
@@ -58,32 +67,39 @@ public class FlockControl : MonoBehaviour {
 		birdControls = new BirdControl[NUM_BIRDS];
 		for (int i = 0; i < NUM_BIRDS; i++) {
 			BirdControl bird = Instantiate<GameObject>(birdPrefab).GetComponent<BirdControl>();
-			birdControls[i] = bird;
+			birdControls [i] = bird;
 		}
 
 		walls = new GameObject[NUM_WALLS];
 		for (int i = 0; i < NUM_WALLS; i++) {
-			walls[i] = Instantiate<GameObject>(wallPrefab);
-			}
+			walls [i] = Instantiate<GameObject>(wallPrefab);
+			walls[i].AddComponent<RectTransform>();
+			walls[i].GetComponent<RectTransform>().sizeDelta = new Vector2(1,1);
+		}
 
 		resetBirds();
-
+		print(Serialize());
 	}
-	
+
 	public string Serialize() {
 		BirdControl.Bird[] birds = new BirdControl.Bird[NUM_BIRDS];
 		for (int i = 0; i < NUM_BIRDS; i++) {
-			birds[i] = birdControls[i].ToStruct();
+			birds [i] = birdControls [i].ToStruct();
+		}
+		WallState[] wallStates = new WallState[walls.Length];
+		for (int i = 0; i < NUM_WALLS; i++) {
+			wallStates[i] = wallToWallState(walls[i]);
 		}
 		WorldState ws = new WorldState();
 		ws.birds = birds;
 		ws.goalPosition = (Vector2)goal.transform.position;
+		ws.walls = wallStates;
 		return JsonUtility.ToJson(ws);
 	}
 
 	public void IncrementGoal() {
 		reachedGoal++;
-		if (reachedGoal==NUM_BIRDS){
+		if (reachedGoal == NUM_BIRDS) {
 			statsControl.PrintStats();
 			resetBirds();
 		}
@@ -94,59 +110,71 @@ public class FlockControl : MonoBehaviour {
 
 		reachedGoal = 0;
 		for (int i = 0; i < NUM_BIRDS; i++) {
-			BirdControl bird = birdControls[i];
+			BirdControl bird = birdControls [i];
 			bird.transform.position = randomPosition();
-			float size = Random.Range(MIN_SIZE,MAX_SIZE);
-			float speed = Random.Range(MIN_SPEED,MAX_SPEED);
-			bird.Setup(size,speed,i);
-			bird.GetComponent<Renderer>().material.color = new Color(Random.Range(.5f,1f),Random.Range(.5f,1f),Random.Range(.5f,1f));
+			float size = Random.Range(MIN_SIZE, MAX_SIZE);
+			float speed = Random.Range(MIN_SPEED, MAX_SPEED);
+			bird.Setup(size, speed, i);
+			bird.GetComponent<Renderer>().material.color = new Color(Random.Range(.5f, 1f), Random.Range(.5f, 1f), Random.Range(.5f, 1f));
 		}
 
 		for (int i = 0; i < NUM_WALLS; i++) {
-			float width = Random.Range(WALL_MIN_WIDTH,WALL_MAX_WIDTH);
-			float area = Random.Range(WALL_MIN_AREA,WALL_MAX_AREA);
-			walls[i].transform.localScale = new Vector3(width,area/width,1f);
-			walls[i].transform.position = randomPosition();
-			walls[i].transform.rotation = Quaternion.Euler(0,0,Random.Range(0f,360f));
+			float width = Random.Range(WALL_MIN_WIDTH, WALL_MAX_WIDTH);
+			float area = Random.Range(WALL_MIN_AREA, WALL_MAX_AREA);
+			walls [i].transform.localScale = new Vector3(width, area / width, 1f);
+			walls [i].transform.position = randomPosition();
+			walls [i].transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+//			RectTransform rectTransform = walls[i].GetComponent<RectTransform>();
 		}
 
-		statsControl.Setup(NUM_BIRDS,MAX_TIME);
+		statsControl.Setup(NUM_BIRDS, MAX_TIME);
 		startTime = Time.time;
 	}
 
 	public void Deserialize(string rawCommand) {
 		// Expected format is a Python list of lists of two numbers ie [[1,2],[3,4]]
 		// Removed the outermost brackets
-		rawCommand = rawCommand.Substring(1,rawCommand.Length-2);
+		rawCommand = rawCommand.Substring(1, rawCommand.Length - 2);
 		// If we know for sure we had more than 1 bird, we could split by '],[' but instead we must split by '['
-		string[] rawSplits = rawCommand.Split(new char[]{'['});
+		string[] rawSplits = rawCommand.Split(new char[]{ '[' });
 
 		for (int i = 0; i < rawSplits.Length; i++) {
 			// The first string will always be "", so the bird we are on is i-1
-			if (rawSplits[i] == "") {
+			if (rawSplits [i] == "") {
 				continue;
 			}
 			// Splitting by ']' and getting the first element in that split gives us 1,2
 			// So split that by ',' to get the raw numbers
-			string[] xy = rawSplits[i].Split(new char[]{']'})[0].Split(new char[]{','});
-			Vector2 accel = new Vector2(float.Parse(xy[0]),float.Parse(xy[1]));
-			birdControls[i-1].SetAcceleration(accel);
+			string[] xy = rawSplits [i].Split(new char[]{ ']' }) [0].Split(new char[]{ ',' });
+			Vector2 accel = new Vector2(float.Parse(xy [0]), float.Parse(xy [1]));
+			birdControls [i - 1].SetAcceleration(accel);
 		}
 	}
 
 
 	public Rect GetWorldBound() {
-		return new Rect(0,0,ROOM_WIDTH,ROOM_HEIGHT);
+		return new Rect(0, 0, ROOM_WIDTH, ROOM_HEIGHT);
 	}
 
 	private Vector3 randomPosition() {
-		return new Vector3(Random.Range(0,ROOM_WIDTH),Random.Range(0,ROOM_HEIGHT),0);
+		return new Vector3(Random.Range(0, ROOM_WIDTH), Random.Range(0, ROOM_HEIGHT), 0);
 	}
 
 	private void Update() {
-		if (Time.time-startTime>MAX_TIME) {
+		if (Time.time - startTime > MAX_TIME) {
 			statsControl.PrintStats();
 			resetBirds();
 		}
+	}
+
+	private WallState wallToWallState(GameObject wall) {
+		Vector3[] corners = new Vector3[4];
+		wall.GetComponent<RectTransform>().GetWorldCorners(corners);
+		WallState ws = new WallState();
+		ws.topLeft = corners[0];
+		ws.topRight = corners[1];
+		ws.bottomLeft = corners[2];
+		ws.bottomRight = corners[3];
+		return ws;
 	}
 }
