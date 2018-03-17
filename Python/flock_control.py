0de733660a7046a197b724e66d0edb1e9a3f0835
Benjamin Sisson <benmsisson@gmail.com>
2018-03-17 19:16:22 -0400

    Added make_grid_2. Which is like make_grid but used the contour instead of the whole area of a shape

--- Python/flock_control.py
+++ Python/flock_control.py
@@ -1,9 +1,10 @@
 import numpy as np
+from scipy import interpolate
 import shapely.geometry as sh
 from timeit import default_timer as timer
 
 will_print = True
-grid_step = .5
+grid_step = .1
 
 
 class FlockControl():
@@ -16,6 +17,8 @@ class FlockControl():
         start = timer()
 
         birds = world_state["birds"]
+        bird_shapes = [sh.Polygon(corner_struct_to_tuples(bird["rectCorners"])) for bird in birds]
+
         goal_pos = xy_dict_to_vector(world_state["goalPosition"])
 
         walls = [corner_struct_to_tuples(wall_struct) for wall_struct in world_state["walls"]]
@@ -23,14 +26,74 @@ class FlockControl():
 
         goal_shape = sh.Point(goal_pos).buffer(world_state["goalDiameter"]/2)
         
-        grid = make_grid(world_state["roomWidth"],world_state["roomHeight"],goal_shape,wall_shapes,birds)
+        # grid = make_grid(world_state["roomWidth"],world_state["roomHeight"],goal_shape,wall_shapes,bird_shapes)
+
+        grid = make_grid_2(world_state["roomWidth"],world_state["roomHeight"],goal_shape,wall_shapes,bird_shapes)
+        # for x in grid:
+        #     print(x)
         will_print = False
 
-        print(timer()-start)
+
+        # for a in wall_shapes[0].boundary:
+        #     print(a)
+
+        print("TIME:",timer()-start)  
+        exit()
+
         return (world_state["generation"],[point_to_goal(bird,goal_pos) for bird in birds])
         
 
-def make_grid(width,height,goal_shape, wall_shapes, birds):
+def mark_boundary(shape,marker,grid):
+    global will_print,grid_step
+    # Mark every point on the line between p1 and p2
+    def points_in_line(p1,p2, axis = 0):
+        print(p1,p2)
+        x_diff = p2[0]-p1[0]
+        y_diff = p2[1]-p1[1]
+        max_diff = max(abs(x_diff),abs(y_diff))
+        max_steps = abs(max_diff/grid_step)
+        
+        assert max_steps > 0
+
+        for s in range(int(max_steps)+1):
+            theta = s / max_steps
+            x = p1[0] + x_diff * theta
+            y = p1[1] + y_diff * theta
+            grid_x = int(x/grid_step)
+            grid_y = int(y/grid_step)
+            if grid_x < 0 or grid_x >= len(grid):
+                continue
+            if grid_y < 0 or grid_y >= len(grid[0]):
+                continue
+            grid[grid_x][grid_y] = marker
+
+ 
+    boundary = shape.boundary.coords
+    for i in range(len(boundary)-1):
+        points_in_line(boundary[i],boundary[i+1])
+
+
+def make_grid_2(width,height,goal_shape,wall_shapes,bird_shapes):
+    global will_print, grid_step
+
+    width_points = int(width/grid_step)
+    height_points = int(height/grid_step)
+    grid = [0]*width_points
+    grid_points = [0]*width_points
+    for x in range(width_points):
+        grid[x] = ['0']*height_points
+
+    mark_boundary(goal_shape,'G',grid)
+
+    for ws in wall_shapes:
+        mark_boundary(ws,'W',grid)
+
+    for i, bs in enumerate(bird_shapes):
+        mark_boundary(bs,'B',grid)
+    return grid
+
+
+def make_grid(width,height,goal_shape, wall_shapes, bird_shapes):
     global will_print, grid_step
 
     # Given a Shapely Shape, fills the interior of the Shape on the grid with the given marker
@@ -59,12 +122,7 @@ def make_grid(width,height,goal_shape, wall_shapes, birds):
         grid[x] = ['0']*height_points
 
 
-    # Convert all birds to shapes
-    bird_shapes = [sh.Polygon(corner_struct_to_tuples(bird["rectCorners"])) for bird in birds]
-
     for i, bs in enumerate(bird_shapes):
-        if not birds[i]["active"]:
-            continue
         fill_bound(bs,("BIRD",i))
     
     fill_bound(goal_shape,("GOAL",))
